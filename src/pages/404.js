1d1d698c1ab257b0e3689347637e992a82516011
brentcobb <brent.tm.cobb@gmail.com>
2020-12-14 21:11:54 -0500

    added 404

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Missing404 = () => {
+  return (
+    <div>
+      <h1>This is my 404 page and not a default one. </h1>
+    </div>
+  )
+}
+
+export default Missing404
